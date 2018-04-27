fd55d7bb9b51858d35a4db5ecb5625654bdb1a41
Andrew McGovern <winedark.mcgovern@gmail.com>
2018-04-27 11:54:01 -0400

    finished basic ajax for fetching user profile and requests

--- js/employee-homepage.js
+++ js/employee-homepage.js
@@ -1,4 +1,3 @@
-// window.onload = onWindowLoad;
 
 // Elements selected from the DOM
 let myRevatureButtons = document.getElementsByClassName('revature-button');
@@ -6,6 +5,15 @@ let logoutButton = document.getElementById('logout-button');
 let submitRequestButton = document.getElementById('submit-request');
 let pendingRequestsEmployeeBtn = document.getElementById('pending-requests-employee-btn');
 let pendingRequestsTable = document.getElementById('pending-requests-table');
+
+let employeeName;
+let employeeEmail;
+let employeeUsername;
+let employeeId;
+
+let locationArray;
+let paramString;
+let userId;
 
 
 // Register Event Listeners
@@ -41,9 +49,64 @@ pendingRequestsEmployeeBtn.addEventListener('click', () => {
   };
 });
 
-// submitRequestButton.addEventListener('click', () => {
-//   let listElement = document.createElement('li');
-//   listElement.innerHTML = "Request #" + count;
-//   pendingRequestsList.appendChild(listElement);
-//   count++;
-// });
+let fetchUserProfile = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://localhost:8080//ers-project/FrontController/user.ajax?user-id=${userId}`);
+  xhr.send();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let objectifiedData = JSON.parse(xhr.responseText);
+      let user = objectifiedData[0];
+
+      employeeName.innerHTML = `${user.firstName} ${user.lastName}`;
+      employeeEmail.innerHTML = user.email;
+      employeeUsername.innerHTML = user.username;
+      employeeId.innerHTML = user.id;
+    }
+  };
+};
+
+let fetchUserRequests = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://localhost:8080//ers-project/FrontController/request/getAllPendingRequestsForEmployee.ajax?user-id=${userId}`);
+  xhr.send();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let requests = JSON.parse(xhr.responseText);
+      requests.forEach(request => {
+        let tableRow = document.createElement('tr');
+
+        let requestIdCell = document.createElement('th');
+        let requestPurposeCell = document.createElement('td');
+        let requestAmountCell = document.createElement('td');
+
+        requestIdCell.innerHTML = request.requestId;
+        requestPurposeCell.innerHTML = request.purpose;
+        requestAmountCell.innerHTML = request.requestAmount;
+
+        tableRow.appendChild(requestIdCell);
+        tableRow.appendChild(requestPurposeCell);
+        tableRow.appendChild(requestAmountCell);
+
+        pendingRequestsTable.appendChild(tableRow);
+      });
+    }
+  };
+};
+
+let initWindow = () => {
+  employeeName = document.getElementById('employee-name');
+  employeeEmail = document.getElementById('employee-email');
+  employeeUsername = document.getElementById('employee-username');
+  employeeId = document.getElementById('employee-id');
+
+  locationArray = window.location.href.split('?');
+  paramString = locationArray[1];
+  userId = paramString.slice(3);
+  console.log(userId);
+
+  fetchUserProfile();
+  fetchUserRequests();
+};
+
+window.onload = initWindow;
